8d6d92c815c3d1b9bfbe04bf5220d645e766878a
clown <menco2001@gmail.com>
2024-08-27 17:27:32 +0200

    cross section

--- function.py
+++ function.py
@@ -15,7 +15,11 @@ def multi_gauss3(x,c0,c1,m1,s1,c2,m2,s2,c3,m3,s3):
     
     return c0+gauss_1+gauss_2+gauss_3
 
-
+def multi_gauss2(x,c0,c1,m1,s1,c2,m2,s2):
+    gauss_1=c1*np.exp(-np.power((x-m1),2.)/(2.*s1**2))
+    gauss_2=c2*np.exp(-np.power((x-m2),2.)/(2.*s2**2))
+    
+    return c0+gauss_1+gauss_2
 
 def line(x, m, q):
     y = m * x + q
